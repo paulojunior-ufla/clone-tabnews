8141a20bc19941a434a57a7de6a2849dd1d8e3da
Paulo Júnior <81873228+paulojunior-ufla@users.noreply.github.com>
2025-03-13 22:52:30 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone do projeto https://www.tabnews.com.br/
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 function Home() {
-    return (
-        <div>
-            <h2>📖 João 3:16</h2>
-            <p>
-                ✨ Porque Deus amou ao mundo de tal maneira que deu o seu Filho unigênito,
-                para que todo o que nele crê 🙏 não pereça, mas tenha a vida eterna 🌿.
-            </p>
-        </div>
-    )
+  return (
+    <div>
+      <h2>📖 João 3:16</h2>
+      <p>
+        ✨ Porque Deus amou ao mundo de tal maneira que deu o seu Filho
+        unigênito, para que todo o que nele crê 🙏 não pereça, mas tenha a vida
+        eterna 🌿.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
