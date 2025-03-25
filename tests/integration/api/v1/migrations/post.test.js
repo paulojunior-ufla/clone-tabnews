ad1133d5e8d28a6be81dfccaf449ce51df803a7d
Paulo Júnior <81873228+paulojunior-ufla@users.noreply.github.com>
2025-03-25 12:31:44 +0000

    improve tests for migrations

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,10 +1,22 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST /api/v1/migrations return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+  const responseGet = await fetch("http://localhost:3000/api/v1/migrations");
+  const responsePost = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-  expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+  expect(responseGet.status).toBe(200);
+  expect(responsePost.status).toBe(200);
 
-  expect(Array.isArray(responseBody)).toBe(true);
+  const responseBodyGet = await responseGet.json();
+  const responseBodyPost = await responsePost.json();
+
+  expect(responseBodyGet).toEqual(responseBodyPost);
 });
